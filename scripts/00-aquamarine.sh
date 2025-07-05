596031b436c0907149b05934d12c4040eb18d872
grassiricardo <grassiricardorg@gmail.com>
2025-07-05 10:09:40 -0300

    fix: add aquamarine

--- scripts/00-aquamarine.sh
+++ scripts/00-aquamarine.sh
@@ -2,18 +2,17 @@
 
 echo "🌊 Compilando e instalando o Aquamarine..."
 
-# Clonar e instalar o hyprwayland-scanner (dependência do Aquamarine)
-git clone https://github.com/hyprwm/hyprwayland-scanner.git
+# Instala dependência hyprwayland-scanner
+git clone --depth=1 https://github.com/hyprwm/hyprwayland-scanner.git
 cd hyprwayland-scanner
-make all
-sudo make install
+cmake -B build -DCMAKE_BUILD_TYPE=Release
+cmake --build build
+sudo cmake --install build
 cd ..
 
-# Clonar e compilar o Aquamarine
-git clone https://github.com/Aylur/Aquamarine.git aquamarine
+# Clona e instala Aquamarine
+git clone --depth=1 https://github.com/Aylur/Aquamarine.git aquamarine
 cd aquamarine
-
-# Compilação padrão CMake
 cmake -B build -DCMAKE_BUILD_TYPE=Release
 cmake --build build
 sudo cmake --install build
