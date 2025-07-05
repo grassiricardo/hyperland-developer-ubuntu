29da17e9b1d9ae6baf1a180bd5c8fc81b5dcd214
grassiricardo <grassiricardorg@gmail.com>
2025-07-05 09:48:44 -0300

    fix: install.ssh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "🔄 Removing any existing local copy..."
+rm -rf hyperland-developer-ubuntu
+
 echo "🔄 Cloning Hyperland setup repo..."
 git clone https://github.com/grassiricardo/hyperland-developer-ubuntu.git
 cd hyperland-developer-ubuntu
